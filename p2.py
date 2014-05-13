24696023dbcb4f9a836c245056639d1929bec521
Girish Kulkarni <kulkarni.girish60@gmail.com>
2014-05-13 13:08:49 +0530

    P2 #added problem statement in comments

--- p2.py
+++ p2.py
@@ -1,3 +1,6 @@
+"""
+    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+"""
 def fibonaci(n = 10):
     x, y = 1, 2
     while x <= n:
